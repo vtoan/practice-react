066731440309cc456cecac045c9d0f9f1bd1486f
Toan Dang Viet <toan.dangviet@nashtechglobal.com>
2024-02-29 13:38:52 +0700

    Change proj to typescript template

--- src/my-scheduler/src/App.tsx
+++ src/my-scheduler/src/App.tsx
@@ -1,13 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button>Hello</Button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
